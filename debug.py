0e885f9b3eba2b7e9b443d7eae903983d175086a
root <root@localhost.localdomain>
2015-09-15 15:48:46 +0200

    Lab 1 and 2

---
+++ debug.py
@@ -0,0 +1,15 @@
+__author__ = 'root'
+
+def sum(a,b):
+    int_a = convert_integer(a)
+    int_b = convert_integer(b)
+
+    result = int_a + int_b
+    return result
+
+def convert_integer(number_string):
+    integer = int(number_string)
+    return integer
+
+
+answer = sum(5,3)
---
+++ test.py
@@ -0,0 +1,7 @@
+import github3
+
+print "Hello world"
+
+i = 100
+
+print i
